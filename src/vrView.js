aa68d32310e7291cadd17fa9f8ac416c071bf9e5
mattia-wsl <mat.ronzoni@gmail.com>
2021-04-12 17:54:55 +0200

    continue porting

--- src/vrView.js
+++ src/vrView.js
@@ -7,6 +7,12 @@ import vtkWidgetManager from "vtk.js/Sources/Widgets/Core/WidgetManager";
 import vtkColorMaps from "vtk.js/Sources/Rendering/Core/ColorTransferFunction/ColorMaps";
 import vtkPiecewiseGaussianWidget from "vtk.js/Sources/Interaction/Widgets/PiecewiseGaussianWidget";
 
+import vtkMouseCameraTrackballRotateManipulator from "vtk.js/Sources/Interaction/Manipulators/MouseCameraTrackballRotateManipulator";
+import vtkMouseCameraTrackballPanManipulator from "vtk.js/Sources/Interaction/Manipulators/MouseCameraTrackballPanManipulator";
+import vtkMouseCameraTrackballZoomManipulator from "vtk.js/Sources/Interaction/Manipulators/MouseCameraTrackballZoomManipulator";
+import vtkMouseRangeManipulator from "vtk.js/Sources/Interaction/Manipulators/MouseRangeManipulator";
+import vtkInteractorStyleManipulator from "vtk.js/Sources/Interaction/Style/InteractorStyleManipulator";
+
 import { getVolumeCenter, createVolumeActor } from "./utils";
 
 export class VRView {
@@ -18,10 +24,14 @@ export class VRView {
     this.renderer = null;
     this.renderWindow = null;
     this.actor = null;
+    this.raysDistance = 1.5; // TODO set/get
 
     // piecewise gaussian widget stuff
     this.PGwidget = null;
     this.gaussians = null;
+
+    this.ww = 0;
+    this.wl = 0;
 
     this.initVR();
 
@@ -54,14 +64,37 @@ export class VRView {
 
     this.renderer.addVolume(actor);
 
+    this.setCamera(actor.getCenter());
+
     this.updateWidget();
     this.setWidgetCallbacks();
     // TODO
-    // setupInteractor(center, phase);
-    // setCamera(center);
+    // - implement a strategy to set rays distance
+    // - setup interactors (ex. blurring or wwwl or crop)
+    // this.setupInteractor();
+
+    this.blurOnInteraction(true);
 
     this.renderer.resetCamera();
     this.renderWindow.render();
+  }
+
+  /**
+   * Set camera lookat point
+   * @param {Array} center - As [x,y,z]
+   */
+  setCamera(center) {
+    this.renderer.resetCamera();
+    this.renderer.getActiveCamera().zoom(1.5);
+    this.renderer.getActiveCamera().elevation(70);
+    this.renderer.getActiveCamera().setViewUp(0, 0, 1);
+    this.renderer
+      .getActiveCamera()
+      .setFocalPoint(center[0], center[1], center[2]);
+    this.renderer
+      .getActiveCamera()
+      .setPosition(center[0], center[1] - 2000, center[2]);
+    this.renderer.getActiveCamera().setThickness(10000);
   }
 
   addLUT(actor) {
@@ -72,11 +105,6 @@ export class VRView {
 
     // set up color transfer function
     lookupTable.applyColorMap(vtkColorMaps.getPresetByName("Cool to Warm"));
-    // hardcode an initial mapping range here.
-    // Normally you would instead use the range from
-    // imageData.getPointData().getScalars().getRange()
-    lookupTable.setMappingRange(0, 256);
-    lookupTable.updateRange();
 
     // set up simple linear opacity function
     // This assumes a data range of 0 -> 256
@@ -93,6 +121,8 @@ export class VRView {
       .getScalars()
       .getRange();
 
+    // TODO generalize: remapping to max/min hist (as bool)
+    range[1] -= 2500;
     lookupTable.setMappingRange(...range);
     lookupTable.updateRange();
 
@@ -224,7 +254,7 @@ export class VRView {
     );
 
     // TODO initilize in a smarter way
-    this.PGwidget.addGaussian(0.5, 0.7, 0.3, -0.02, -0.1); // x, y, ampiezza, sbilanciamento, andamento
+    this.PGwidget.addGaussian(0.33, 0.7, 0.3, -0.02, -0.1); // x, y, ampiezza, sbilanciamento, andamento
     this.PGwidget.applyOpacity(this.ofun);
     this.PGwidget.setColorTransferFunction(this.ctfun);
     this.ctfun.onModified(() => {
@@ -260,5 +290,106 @@ export class VRView {
 
   setSampleDistance(distance) {
     this.actor.getMapper().setSampleDistance(distance);
+  }
+
+  /**
+   * Toggle blurring on interaction (Increase performance)
+   * @param {bool} toggle - if true, blur on interaction
+   */
+  blurOnInteraction(toggle) {
+    let interactor = this.renderWindow.getInteractor();
+    let mapper = this.actor.getMapper();
+
+    if (toggle) {
+      interactor.onLeftButtonPress(() => {
+        mapper.setSampleDistance(this.raysDistance * 5);
+      });
+
+      interactor.onLeftButtonRelease(() => {
+        mapper.setSampleDistance(this.raysDistance);
+        this.renderWindow.render();
+      });
+    } else {
+      interactor.onLeftButtonPress(() => {
+        mapper.setSampleDistance(this.raysDistance);
+      });
+
+      interactor.onLeftButtonRelease(() => {
+        mapper.setSampleDistance(this.raysDistance);
+      });
+    }
+  }
+
+  /**
+   * Init wwwl interactor
+   * LEFT DRAG : rotate
+   * CTRL: pan
+   * SCROLL: zoom
+   * SHIFT + LEFT DRAG : wwwl
+   */
+  setupInteractor() {
+    // TODO setup from user
+    const rotateManipulator = vtkMouseCameraTrackballRotateManipulator.newInstance(
+      { button: 1 }
+    );
+    const panManipulator = vtkMouseCameraTrackballPanManipulator.newInstance(
+      { button: 1, control: true } // on ctrl press
+    );
+    const zoomManipulator = vtkMouseCameraTrackballZoomManipulator.newInstance(
+      { scrollEnabled: true } // on scroll
+    );
+    const rangeManipulator = vtkMouseRangeManipulator.newInstance(
+      { button: 1, shift: true } // on shift press
+    );
+
+    let self = this;
+
+    function getWL() {
+      return self.wl;
+    }
+
+    function getWW() {
+      return self.ww;
+    }
+
+    function setWL(v) {
+      let wl = self.wl + (v - self.wl) / 25;
+
+      self.wl = wl;
+
+      // TODO update widget
+      // let gaussians = self.PGwidget.getGaussians().slice(); // NOTE: slice() to clone!
+      // gaussians[0].position = wl + self.wl; //TODO:foreach
+      // self.PGwidget.setGaussians(gaussians);
+    }
+
+    function setWW(v) {
+      let ww = self.ww + (v - self.ww) / 5;
+
+      self.ww = ww;
+
+      // TODO update widget
+      // let gaussians = self.PGwidget.getGaussians().slice(); // NOTE: slice() to clone!
+      // gaussians[0].width = ww * self.ww; //TODO foreach
+      // self.PGwidget.setGaussians(gaussians);
+    }
+
+    rangeManipulator.setVerticalListener(-1, 1, 0.001, getWL, setWL);
+    rangeManipulator.setHorizontalListener(0.1, 2.1, 0.001, getWW, setWW);
+
+    const interactorStyle = vtkInteractorStyleManipulator.newInstance();
+    interactorStyle.addMouseManipulator(rangeManipulator);
+    interactorStyle.addMouseManipulator(rotateManipulator);
+    interactorStyle.addMouseManipulator(panManipulator);
+    interactorStyle.addMouseManipulator(zoomManipulator);
+    interactorStyle.setCenterOfRotation(this.actor.getCenter());
+    this.renderWindow.getInteractor().setInteractorStyle(interactorStyle);
+  }
+
+  /**
+   * Destroy webgl content and release listeners
+   */
+  destroy() {
+    this.renderWindow.delete();
   }
 }
