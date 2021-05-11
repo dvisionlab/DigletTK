0d343efb2cbadadb2a8c669c58653529d61341e7
mattia-wsl <mat.ronzoni@gmail.com>
2021-05-11 15:26:40 +0200

    update vtk.js@18 and solve breaking changes issues

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+// NOTE: this is necessary as workaround to this issue:
+// https://github.com/Kitware/vtk-js/issues/1882
+import "regenerator-runtime/runtime";
+
+// Load the rendering pieces we want to use (for both WebGL and WebGPU)
+import "vtk.js/Sources/Rendering/Profiles/Volume";
+
 import { VRView } from "./vrView";
 import { MPRManager } from "./mprManager";
 import { loadDemoSerieWithLarvitar, buildVtkVolume } from "./utils";
