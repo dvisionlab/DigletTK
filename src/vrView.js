1051c97edd47f7da708383ba652792d378c5175d
mattia-wsl <mat.ronzoni@gmail.com>
2022-03-25 16:42:45 +0100

    move render passes to proper file

--- src/vrView.js
+++ src/vrView.js
@@ -24,6 +24,7 @@ import { createVolumeActor, getCroppingPlanes } from "./utils/utils";
 import { applyStrategy } from "./utils/strategies";
 
 import { createPreset } from "./utils/colormaps";
+import { getRenderPass } from "./renderPasses";
 
 // Add custom presets
 vtkColorMaps.addPreset(createPreset());
@@ -292,62 +293,7 @@ export class VRView {
   set edgeEnhancement([type, value]) {
     console.log(value);
 
-    function getConvolutionPass(kernel, kernelDimension, delegates = null) {
-      const convolutionPass = vtkConvolution2DPass.newInstance();
-      if (delegates !== null) {
-        convolutionPass.setDelegates(delegates);
-      }
-      convolutionPass.setKernelDimension(kernelDimension);
-      convolutionPass.setKernel(kernel);
-      return convolutionPass;
-    }
-
-    function getEdgeEnhancement1Pass(k, delegates = null) {
-      return getConvolutionPass(
-        [0, -k, 0, -k, 1 + 4 * k, -k, 0, -k, 0],
-        3,
-        delegates
-      );
-    }
-
-    function getEdgeEnhancement2Pass(k, delegates = null) {
-      return getConvolutionPass(
-        [-k, -k, -k, -k, 1 + 8 * k, -k, -k, -k, -k],
-        3,
-        delegates
-      );
-    }
-
-    function getEdgeEnhancement3Pass(k, delegates = null) {
-      return getConvolutionPass(
-        [-k, -2 * k, -k, -2 * k, 1 + 12 * k, -2 * k, -k, -2 * k, -k],
-        3,
-        delegates
-      );
-    }
-
-    function getGaussianBlurPass(delegates = null) {
-      return getConvolutionPass([1, 2, 1, 2, 4, 2, 1, 2, 1], 3, delegates);
-    }
-
-    let renderPass = vtkForwardPass.newInstance();
-    let gaussianRenderPass = vtkForwardPass.newInstance();
-    let gaussianPass = getGaussianBlurPass([gaussianRenderPass]);
-
-    switch (type) {
-      case 1:
-        renderPass = getEdgeEnhancement1Pass(value, [gaussianPass, renderPass]);
-        break;
-      case 2:
-        renderPass = getEdgeEnhancement2Pass(value, [gaussianPass, renderPass]);
-        break;
-      case 3:
-        renderPass = getEdgeEnhancement3Pass(value, [gaussianPass, renderPass]);
-        break;
-      default:
-        console.warn("no edge enhancement of type ", type);
-        return;
-    }
+    let renderPass = getRenderPass(type);
 
     let view = this.renderWindow.getViews()[0];
     view.setRenderPasses([renderPass]);
